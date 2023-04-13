b320eed72717a839f8cab9ab8d7b5e137ed5f688
Jun Hui <junhui.tan.2021@scis.smu.edu.sg>
2023-04-14 02:36:04 +0800

    fixed black boundary

--- app/src/main/java/com/example/CS205/map/MapLayout.java
+++ app/src/main/java/com/example/CS205/map/MapLayout.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class MapLayout {
     public static final int TILE_WIDTH_PIXELS = 64;
     public static final int TILE_HEIGHT_PIXELS = 64;
-    public static final int NUMBER_OF_ROW_TILES = 60;
-    public static final int NUMBER_OF_COLUMN_TILES = 60;
+    public static final int NUMBER_OF_ROW_TILES = 100;
+    public static final int NUMBER_OF_COLUMN_TILES = 100;
 
     private int[][] layout;
 
@@ -83,9 +83,9 @@ public class MapLayout {
 //                {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2}
 //        };
         Random rand = new Random();
-        layout = new int[60][60];
-        for (int i = 0; i < 60; i++) {
-            for (int j = 0; j < 60; j++) {
+        layout = new int[100][100];
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
                 int random = rand.nextInt(1000);
                 if (random < 100){
                     layout[i][j] = 4;
