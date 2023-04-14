b5c2114146ebd0bcdd0d17435a48b626b9406bdd
Shao Dong <sdlee.2021@scis.smu.edu.sg>
2023-04-15 01:06:11 +0800

    changed background and bgm

--- app/src/main/java/com/example/CS205/MainActivity.java
+++ app/src/main/java/com/example/CS205/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends Activity {
 
         //BGM
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.openingmusic);
+        mediaPlayer = MediaPlayer.create(this, R.raw.wintermusic);
         mediaPlayer.setLooping(true);
         mediaPlayer.start();
 
