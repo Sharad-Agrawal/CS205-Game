95578c0cf85794f60e0ac042ebd81c68969cc5aa
Shao Dong <sdlee.2021@scis.smu.edu.sg>
2023-04-14 10:53:21 +0800

    added more changes to spell

--- app/src/main/java/com/example/CS205/gameobject/Spell.java
+++ app/src/main/java/com/example/CS205/gameobject/Spell.java
@@ -1,30 +1,130 @@
 package com.example.CS205.gameobject;
 
 import android.content.Context;
-import android.support.v4.content.ContextCompat;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.media.MediaPlayer;
 
+import com.example.CS205.GameDisplay;
 import com.example.CS205.GameLoop;
 import com.example.CS205.R;
 
+import java.util.Random;
+
 public class Spell extends Circle {
-    public static final double SPEED_PIXELS_PER_SECOND = 800.0;
+
+    private static final double SPEED_PIXELS_PER_SECOND = 800.0;
     private static final double MAX_SPEED = SPEED_PIXELS_PER_SECOND / GameLoop.MAX_UPS;
 
+    private MediaPlayer soundEffect;
+
+    private Paint outerCirclePaint;
+    private Paint innerCirclePaint;
+    private Paint sparklePaint;
+
+    private Random random = new Random();
+
+    private float gradientRadius = 0;
+    private boolean increasing = true;
+
     public Spell(Context context, Player spellcaster) {
-        super(
-            context,
-            ContextCompat.getColor(context, R.color.spell),
-            spellcaster.getPositionX(),
-            spellcaster.getPositionY(),
-      25
-        );
-        velocityX = spellcaster.getDirectionX()*MAX_SPEED;
-        velocityY = spellcaster.getDirectionY()*MAX_SPEED;
+        super(context, Color.YELLOW, spellcaster.getPositionX(), spellcaster.getPositionY(), 25);
+
+        // Set paint style
+        paint.setStyle(Paint.Style.FILL);
+
+        // Add a blur mask filter to enhance the glowing effect
+        paint.setMaskFilter(new android.graphics.BlurMaskFilter(10, android.graphics.BlurMaskFilter.Blur.NORMAL));
+
+        // Set velocity based on the direction the player is facing
+        velocityX = spellcaster.getDirectionX() * MAX_SPEED;
+        velocityY = spellcaster.getDirectionY() * MAX_SPEED;
+
+        soundEffect = MediaPlayer.create(context, R.raw.energypulse);
+        soundEffect.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                soundEffect.release();
+            }
+        });
+        soundEffect.start();
+
+        // Set up paint objects for outer and inner circles and sparkle effects
+        outerCirclePaint = new Paint();
+        outerCirclePaint.setStyle(Paint.Style.STROKE);
+        outerCirclePaint.setStrokeWidth(2);
+        outerCirclePaint.setColor(Color.WHITE);
+        outerCirclePaint.setAlpha(200);
+
+        innerCirclePaint = new Paint();
+        innerCirclePaint.setStyle(Paint.Style.FILL);
+        innerCirclePaint.setColor(Color.YELLOW);
+        innerCirclePaint.setAlpha(150);
+
+        sparklePaint = new Paint();
+        sparklePaint.setStyle(Paint.Style.FILL);
+        sparklePaint.setColor(Color.WHITE);
+        sparklePaint.setAlpha(200);
     }
 
     @Override
     public void update() {
-        positionX = positionX + velocityX;
-        positionY = positionY + velocityY;
+        positionX += velocityX;
+        positionY += velocityY;
+
+        // Update gradient radius for pulsating animation
+        if (increasing) {
+            gradientRadius += 2;
+        } else {
+            gradientRadius -= 2;
+        }
+        if (gradientRadius >= 75) {
+            increasing = false;
+        }
+        if (gradientRadius <= 50) {
+            increasing = true;
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas, GameDisplay gameDisplay) {
+        canvas.save();
+
+        // Add outer circle and inner circle with a gradient effect
+        RadialGradient gradient = new RadialGradient(
+                (float) gameDisplay.gameToDisplayCoordinatesX(positionX),
+                (float) gameDisplay.gameToDisplayCoordinatesY(positionY),
+                gradientRadius,
+                Color.YELLOW,
+                Color.TRANSPARENT,
+                Shader.TileMode.CLAMP
+        );
+        innerCirclePaint.setShader(gradient);
+        canvas.drawCircle(
+                (float) gameDisplay.gameToDisplayCoordinatesX(positionX),
+                (float) gameDisplay.gameToDisplayCoordinatesY(positionY),
+                (float) radius, innerCirclePaint
+        );
+        canvas.drawCircle(
+                (float) gameDisplay.gameToDisplayCoordinatesX(positionX),
+                (float) gameDisplay.gameToDisplayCoordinatesY(positionY),
+                (float) radius,
+                outerCirclePaint
+        );
+
+        // Add sparkle effects
+        for (int i = 0; i < 10; i++) {
+            canvas.drawCircle(
+                    (float) gameDisplay.gameToDisplayCoordinatesX(positionX + random.nextInt((int) radius * 2) - radius),
+                    (float) gameDisplay.gameToDisplayCoordinatesY(positionY + random.nextInt((int) radius * 2) - radius),
+                    random.nextInt(3),
+                    sparklePaint
+            );
+        }
+
+        canvas.restore();
     }
 }
