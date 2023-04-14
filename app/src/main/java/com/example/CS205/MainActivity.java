30880ed43aa53d6b4dac12c0547c0b92a7c0ebf8
Shao Dong <sdlee.2021@scis.smu.edu.sg>
2023-04-14 12:26:30 +0800

    moving background

--- app/src/main/java/com/example/CS205/MainActivity.java
+++ app/src/main/java/com/example/CS205/MainActivity.java
@@ -6,9 +6,14 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.media.MediaPlayer;
+import android.animation.ValueAnimator;
+import android.view.animation.LinearInterpolator;
+
+
 
 
 import com.example.CS205.network.ApiInterface;
@@ -31,9 +36,9 @@ public class MainActivity extends Activity {
     private MediaPlayer mediaPlayer;
 
     EditText nameInput;
-
-
     Animation animBlink;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +49,25 @@ public class MainActivity extends Activity {
 
 //        Map<String, Integer> leaderboard = NetUtility.getLeaderboard();
 //        boolean saveSuccess = NetUtility.saveScoreToLeaderboard("CS205", 1000);
+
+        final ImageView backgroundOne = (ImageView) findViewById(R.id.background1);
+        final ImageView backgroundTwo = (ImageView) findViewById(R.id.background2);
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(10000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = backgroundOne.getWidth();
+                final float translationX = width * progress;
+                backgroundOne.setTranslationX(translationX);
+                backgroundTwo.setTranslationX(translationX - width);
+            }
+        });
+        animator.start();
+
 
         //BGM
 
